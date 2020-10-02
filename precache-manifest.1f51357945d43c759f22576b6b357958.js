e75f97f7045bcc3ad61b396e089c343a7f7c92f3
Dina <diana.liubarets@hotmail.com>
2020-10-01 21:53:19 -0400

    Updates

---
+++ precache-manifest.1f51357945d43c759f22576b6b357958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aee96528ba3bbe04d757ee56fcefb60",
+    "url": "/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "4d98f6b295beb56f18b9",
+    "url": "/EmployeeDirectory/static/css/main.7644f647.chunk.css"
+  },
+  {
+    "revision": "0bbd9bb8d6ba5ea21f54",
+    "url": "/EmployeeDirectory/static/js/2.353689b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmployeeDirectory/static/js/2.353689b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d98f6b295beb56f18b9",
+    "url": "/EmployeeDirectory/static/js/main.568775f6.chunk.js"
+  },
+  {
+    "revision": "635dfe64796a6b3c7309",
+    "url": "/EmployeeDirectory/static/js/runtime-main.4c6ef073.js"
+  }
+]);
