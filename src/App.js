7db5f390b64a2245fe3701a878ab52b92b9707f8
Dina <diana.liubarets@hotmail.com>
2020-10-02 20:30:34 -0400

    set app header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
@@ -7,56 +7,57 @@ import Footer from "./components/Footer";
 import employees from "./db/employees.json";
 
 function App() {
-    const [ searchTerm, setSearchTerm ] = useState("");
-    const [ sorted, setSorted] = useState(false);
-    const [ data, setEmployees ] = useState(employees);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sorted, setSorted] = useState(false);
+  const [data, setEmployees] = useState(employees);
 
-    function handleSearchTerm(event)  {
-        setSearchTerm(event.target.value)
+  function handleSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  function handleSortByName() {
+    // sort array ascending or descending by first name
+    if (!sorted) {
+      setEmployees(data.sort((a, b) => (a.name > b.name ? 1 : -1)));
+      setSorted(true);
+    } else {
+      setEmployees(data.sort((a, b) => (a.name > b.name ? -1 : 1)));
+      setSorted(false);
     }
+  }
 
-    function handleSortByName() {
-        // sort array ascending or descending by first name
-        if (!sorted) {
-            setEmployees(data.sort((a, b) => (a.name > b.name) ? 1 : -1));
-            setSorted(true);
-        } else {
-            setEmployees(data.sort((a, b) => (a.name > b.name) ? -1 : 1));
-            setSorted(false);
-        }
+  function handleSortByDept() {
+    // sort array ascending or descending by dept name
+    if (!sorted) {
+      setEmployees(data.sort((a, b) => (a.department > b.department ? 1 : -1)));
+      setSorted(true);
+    } else {
+      setEmployees(data.sort((a, b) => (a.department > b.department ? -1 : 1)));
+      setSorted(false);
     }
+  }
 
-    function handleSortByDept() {
-        // sort array ascending or descending by dept name
-        if (!sorted) {
-            setEmployees(data.sort((a, b) => (a.department > b.department) ? 1 : -1));
-            setSorted(true);
-        } else {
-            setEmployees(data.sort((a, b) => (a.department > b.department) ? -1 : 1));
-            setSorted(false);
-        }
-    }
-
-    // the filteredEmployees variable only stores employee names that start with with the matching string you type
-    const filteredEmployees = data.filter(employee => employee.name.toLowerCase().startsWith(searchTerm.toLowerCase()));
-    return (
-        <div>
-            <Header/>
-            <Layout>
-                <p className="mb-16 text-md">Search for an employee or sort by Name or Category.</p>
-                {/*the handleSearchTerm method and searchTerm state get passed down to the Navigation component via props with the onSearch and searchTerm props*/}
-                <Navigation
-                    onSearch={handleSearchTerm}
-                    searchTerm={searchTerm}
-                    handleSortByName={handleSortByName}
-                    handleSortByDept={handleSortByDept}
-                />
-                {/* the employees array gets the filteredEmployees data via the data prop */}
-                <EmployeeCardList data={filteredEmployees}/>
-                <Footer/>
-            </Layout>
-        </div>
-    )
+  // the filteredEmployees variable only stores employee names that start with with the matching string you type
+  const filteredEmployees = data.filter((employee) =>
+    employee.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
+  return (
+    <div>
+      <Header />
+      <Layout>
+        {/*the handleSearchTerm method and searchTerm state get passed down to the Navigation component via props with the onSearch and searchTerm props*/}
+        <Navigation
+          onSearch={handleSearchTerm}
+          searchTerm={searchTerm}
+          handleSortByName={handleSortByName}
+          handleSortByDept={handleSortByDept}
+        />
+        {/* the employees array gets the filteredEmployees data via the data prop */}
+        <EmployeeCardList data={filteredEmployees} />
+        <Footer />
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 function Header() {
-    return (
-        <div className="bg-white shadow text-gray-800">
-            <header className="container max-w-6xl px-10 py-3 mx-auto flex justify-between items-center">
-                <div className="flex items-center">
-                    <h1 className="title text-4xl text-gray-800 mt-1">Employee Directory</h1>
-                </div>
-            </header>
+  return (
+    <div className="bg-white shadow text-gray-800">
+      <header className="container max-w-6xl px-10 py-3 mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <h1 className="title text-4xl text-gray-800 mt-1">
+            Employee Directory
+          </h1>
         </div>
-    )
+        <p className="mb-16 text-md">
+          Search for an employee or sort by Name or Category.
+        </p>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
